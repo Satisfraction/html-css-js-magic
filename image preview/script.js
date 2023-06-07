f241676341585908d66822c62f794416439cca40
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-06-07 21:44:07 +0200

    Update script.js
    add comments

--- image preview/script.js
+++ image preview/script.js
@@ -1,8 +1,14 @@
+// Get the reference to the image element with the ID "img"
 let img = document.getElementById("img");
+
+// Get the reference to the input element with the ID "input"
 let input = document.getElementById("input");
 
+// Add an event listener to the input element, which listens for a change event
 input.onchange = (e) => {
+    // Check if any file is selected
     if (input.files[0]) {
+        // Set the source of the image element to the URL of the selected file
         img.src = URL.createObjectURL(input.files[0]);
     }
 }
