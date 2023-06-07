3706ef36843c1bb71c2e47b05b0eed4ff3f854ae
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-06-07 21:45:09 +0200

    Update script.js
    add comments

--- navbar with icons/script.js
+++ navbar with icons/script.js
@@ -1,28 +1,32 @@
+// This function toggles the mobile menu by adding or removing the 'open' class from the menu.
 function toggleMobileMenu() {
-    const menu = document.querySelector('.navigation ul');
-    menu.classList.toggle('open');
+    const menu = document.querySelector('.navigation ul');        // Selects the menu
+    menu.classList.toggle('open');        // Toggles the 'open' class
 }
 
+// This function toggles the mobile menu when the user clicks on the menu icon.
 function highlightCurrentPageLink() {
-    const links = document.querySelectorAll('.navigation ul li a');
-    const currentUrl = window.location.href;
+    const links = document.querySelectorAll('.navigation ul li a'); // Selects all the links
+    const currentUrl = window.location.href;    // Gets the current URL
 
+    // Iterates over the links
     links.forEach(link => {
-        if (link.href === currentUrl) {
-            link.classList.add('active');
+        if (link.href === currentUrl) {    // If the link is the current URL
+            link.classList.add('active');    // Add the 'active' class
         } else {
-            link.classList.remove('active');
+            link.classList.remove('active');    // Remove the 'active' class
         }
     });
 }
 
+// This function scrolls to the section when the user clicks on a link.
 function scrollToSection(event) {
-    event.preventDefault();
-    const sectionId = event.target.getAttribute('href');
-    const section = document.querySelector(sectionId);
+    event.preventDefault();    // Prevents the default action
+    const sectionId = event.target.getAttribute('href');    // Gets the section ID
+    const section = document.querySelector(sectionId);    // Selects the section
 
+    // Scrolls to the section
     section.scrollIntoView({
-        behavior: 'smooth'
+        behavior: 'smooth'    // Smooth scrolling
     });
 }
-
