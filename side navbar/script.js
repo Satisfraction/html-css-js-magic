371e41ce48b9b4516542a42ab6f5e9be0579f511
Satisfraction <122011782+Satisfraction@users.noreply.github.com>
2023-06-08 02:33:15 +0200

    Update script.js
    add comments

--- side navbar/script.js
+++ side navbar/script.js
@@ -1,9 +1,15 @@
-// Add active class to clicked element
+// Get the navbar element and all anchor tags within it
 const navbar = document.querySelector(".navbar");
 const links = navbar.querySelectorAll("a");
+
+// Add a click event listener to each anchor tag
 links.forEach(link => {
   link.addEventListener("click", () => {
+
+    // Remove the "active" class from all anchor tags
     links.forEach(link => link.classList.remove("active"));
+
+    // Add the "active" class to the clicked anchor tag
     link.classList.add("active");
   });
 });
